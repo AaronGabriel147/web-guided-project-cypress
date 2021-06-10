afbe3d53798ff445c9bc0d33a1c4d30d13947850
Timothy Roy <timothy.roy@protonmail.ch>
2021-06-09 21:33:08 -0500

    Added more assertions and another test

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -7,8 +7,23 @@ describe('Quotes App', () => {
         cy.visit('http://localhost:1234')
     })
 
-    // 'it' is the keyword to create tests
-    it('sanity checks', () => {})
+    const submitButton = () => cy.get('button[id="submitBtn"]')
 
+    // 'it' is the keyword to create tests
+    it('sanity checks', () => {
+        // to create an assertion, use 'expect'
+        expect(5).to.equal(5) // 5 === 5
+        expect(1 + 2).to.equal(3) // 1 + 2 === 3
+        expect({}).to.eql({}) // will this pass? with eql yes, with equal
+        expect({}).not.to.equal({}) // not triple equal
+    })
+
+    it("the proper elements exist", () => {
+        cy.get('input[name="text"]').should('exist')
+        cy.get('input[name="ladygaga"]').should('not.exist')
+
+        submitButton().should('exist')
+        cy.get('button[id="cancelBtn"]').should('exist')
+    })
 
 })

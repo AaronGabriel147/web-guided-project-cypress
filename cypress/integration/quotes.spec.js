cfd7d9b5bbce0f7f2d4a29b19156c16c22cd114c
Timothy Roy <timothy.roy@protonmail.ch>
2021-06-09 21:04:24 -0500

    Started our test file! so far it just visits our app

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,14 @@
 // write tests here
+
+describe('Quotes App', () => {
+    // schedule something to run before each test
+    // start each test with a blank slate
+    beforeEach(() => {
+        cy.visit('http://localhost:1234')
+    })
+
+    // 'it' is the keyword to create tests
+    it('sanity checks', () => {})
+
+
+})

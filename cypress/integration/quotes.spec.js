6e2d36bd013db596a52d73ee68754d1d151ba26b
Timothy Roy <timothy.roy@protonmail.ch>
2021-06-09 22:06:24 -0500

    Added more tests inside a nested describe)

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,5 +1,12 @@
 // write tests here
 
+// Most teams don't have enough tests
+// they will thank you for adding tests
+// unit tests: test a single function, or its output
+// integration tests: test two functions working together
+// end-to-end, aka e2e: what we're doing now
+
+
 describe('Quotes App', () => {
     // schedule something to run before each test
     // start each test with a blank slate
@@ -8,6 +15,9 @@ describe('Quotes App', () => {
     })
 
     const submitButton = () => cy.get('button[id="submitBtn"]')
+    const cancelButton = () => cy.get('button[id="cancelBtn"]')
+    const textInput = () => cy.get('input[name="text"]')
+    const authorInput = () => cy.get('input[name="author"]') 
 
     // 'it' is the keyword to create tests
     it('sanity checks', () => {
@@ -19,11 +29,38 @@ describe('Quotes App', () => {
     })
 
     it("the proper elements exist", () => {
-        cy.get('input[name="text"]').should('exist')
+        textInput().should('exist')
         cy.get('input[name="ladygaga"]').should('not.exist')
 
         submitButton().should('exist')
-        cy.get('button[id="cancelBtn"]').should('exist')
+        cancelButton().should('exist')
     })
 
+   describe("filling in inputs and then cancelling", () => {
+    it('submit button is disabled', () => {
+        submitButton().should('be.disabled')
+        submitButton().should('not.be.enabled')
+    })
+
+    // write a test to check that the cancel button is enabled
+    it('cancel button is enabled', () => {
+        cancelButton().should('be.enabled')
+    })
+
+    it('can type inside the text input', () => {
+        textInput()
+        .should('have.value', "")
+        .type("Be kind to yourself and others")
+        .should('have.value', "Be kind to yourself and others")
+    })
+
+    it("can type inside the author input", () => {
+          authorInput()
+          .should("have.value", "")
+          .type("George Washington Carver")
+          .should("have.value", "George Washington Carver");
+      })
+   })
+
 })
+
